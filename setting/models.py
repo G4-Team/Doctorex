3b6c2b481f777e9a9ed523a5711933d3829edaab
Ramon <ramin0703@gmail.com>
2024-07-10 01:04:46 +0330

    no migration commit

--- setting/models.py
+++ setting/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Specialty(models.Model):
+    specialty = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.specialty
+
+    class Meta:
+        verbose_name_plural = "Specialties"
--- user/models.py
+++ user/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from setting.models import Specialty
 
-# Create your models here.
+
+class Patient(models.Model):
+    balance = models.IntegerField(default=100_000)
+
+
+class Doctor(models.Model):
+    visit_cost = models.IntegerField(default=100_000)
+    clinic_address = models.CharField(max_length=255)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+
+
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, email, username, first_name, last_name, password, **extra_fields):
+        if not email:
+            raise ValueError('you must provide an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, first_name=first_name, last_name=last_name, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, username, first_name, last_name, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, username, first_name, last_name, password, **extra_fields)
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=50)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    password = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True, blank=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, null=True, blank=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
