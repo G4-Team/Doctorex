8c8adadb1cf890648a6c8ece6cd7d7e8589a3ef0
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-10 14:53:30 +0330

    refactor: django structure
    refactor django settings, environ variables and requriemnts
    
    Refs: #11

---
+++ doctorex/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = True
+
+
+SECRET_KEY = 'django-insecure-local-secret-key'
+
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"] 
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
