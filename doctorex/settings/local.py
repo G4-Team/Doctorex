ea44cdba4eeeb7dc2004b7326d03abd2577314ff
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-10 18:22:57 +0330

    fix: change BASE_DIR

--- doctorex/settings/local.py
+++ doctorex/settings/local.py
@@ -3,15 +3,15 @@ from .base import *
 DEBUG = True
 
 
-SECRET_KEY = 'django-insecure-local-secret-key'
+SECRET_KEY = "django-insecure-local-secret-key"
 
 INSTALLED_APPS += ["debug_toolbar"]
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"] 
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
