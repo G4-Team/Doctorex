2039b209b0a1c2dbb0e0cb564f56e60d1628bb34
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-11 18:31:07 +0330

    feat(setting app)!: add image field to Specialty model

--- setting/models.py
+++ setting/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 
+def specialty_image_directory_path(instance, filename):
+    return "specialties/{0}".format(instance.specialty)
+
+
 class Specialty(models.Model):
     specialty = models.CharField(max_length=100)
+    image = models.ImageField(
+        upload_to=specialty_image_directory_path, blank=False, null=True
+    )
 
     def __str__(self):
         return self.specialty
