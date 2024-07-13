89b5e13be16a2b1137d58ae663f3351034c95ebe
SHK <shahryar.karimi1384@gmail.com>
2024-07-13 18:54:29 +0330

    Search View and Url

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('/search/<string:search_content>', views.SearchView.as_view(), name='search'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+from user.models import Doctor
+
 
 class IndexView(View):
     def get(self, request):
         return render(request, "reservation/index.html")
+
+
+class SearchView(View):
+    def get(self, request, search_content):
+        doctors = Doctor.objects.filter(name__icontains=search_content)
+        return render(request, "reservation/search.html", {"doctors": doctors})
