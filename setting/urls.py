d360c708823f53424162165d12cbaae6da171655
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-14 19:21:52 +0330

    feat: add specialty mvt

--- setting/urls.py
+++ setting/urls.py
@@ -6,4 +6,7 @@ app_name = "setting"
 
 urlpatterns = [
     path("specialties/", views.SpecialtyListView.as_view(), name="specialties-list"),
+    path(
+        "specialty/<slug:slug>/", view=views.SpecialtyView.as_view(), name="specialty"
+    ),
 ]
