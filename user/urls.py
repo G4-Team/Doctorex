f55791e0428c9b836f4718a1b984bb796ff8d31f
Ramon <ramin0703@gmail.com>
2024-07-15 10:03:29 +0330

    feature: add logout feature change user/view url and navbar template to add logout feature. Refs: #24

--- user/urls.py
+++ user/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
     path('signin/', views.SigninView.as_view(), name='signin'),
     path("doctor/", include(doctor_urls), name="doctor"),
+    path('signout', views.SignoutView.as_view(), name='signout'),
 ]
