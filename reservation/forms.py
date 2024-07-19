b8cf7f96df269416a752fff94ab1451eebfdb812
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-19 21:52:49 +0330

    fix: comment problem

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+
 from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    doctor_id = forms.IntegerField(required=True)
+
     class Meta:
         model = Comment
-        fields = ['title', 'text', 'score']
+        fields = ["title", "text", "score"]
--- reservation/views.py
+++ reservation/views.py
@@ -1,10 +1,11 @@
 from django.contrib import messages
 from django.db.models import Avg
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 
 from user.models import Doctor
+
 from .forms import CommentForm
 from .models import Comment, Reservation
 
@@ -16,10 +17,12 @@ class IndexView(View):
 
 class SearchView(View):
     def get(self, request, search_content):
-        doctors = (Doctor.objects.filter(account__first_name__icontains=search_content) |
-                   Doctor.objects.filter(specialty__specialty__icontains=search_content) |
-                   Doctor.objects.filter(account__last_name__icontains=search_content))
-        doctors.annotate(average_score=Avg('visittime__reservation__comments__score'))
+        doctors = (
+            Doctor.objects.filter(account__first_name__icontains=search_content)
+            | Doctor.objects.filter(specialty__specialty__icontains=search_content)
+            | Doctor.objects.filter(account__last_name__icontains=search_content)
+        )
+        doctors.annotate(average_score=Avg("visittime__reservation__comments__score"))
         return render(request, "reservation/search.html", {"doctors": doctors})
 
 
@@ -29,14 +32,15 @@ class CommentView(View):
 
         if form.is_valid():
             comment = form.save(commit=False)
+            comment.doctor_id = int(form.cleaned_data["doctor_id"])
             comment.author = request.user
-            comment.reservation = get_object_or_404(Reservation, id=request.POST.get('time'))
+            comment.reservation = get_object_or_404(
+                Reservation, id=request.POST.get("time")
+            )
             comment.save()
-            messages.success(request, 'نظر شما با موفقیت ثبت گردید')
-            previous_url = request.META.get('HTTP_REFERER')
+            messages.success(request, "نظر شما با موفقیت ثبت گردید")
+            previous_url = request.META.get("HTTP_REFERER")
             return HttpResponseRedirect(previous_url)
-
-        print(form.errors)
 
 
 class CommentEditView(View):
@@ -44,21 +48,21 @@ class CommentEditView(View):
         comment = get_object_or_404(Comment, pk=comment_id)
 
         if comment.author != request.user:
-            return redirect('index')
+            return redirect("index")
 
-        return render(request, 'comment.html', {'comment': comment})
+        return render(request, "comment.html", {"comment": comment})
 
     def post(self, request, comment_id):
         comment = get_object_or_404(Comment, pk=comment_id)
 
         if comment.author != request.user:
-            return redirect('index')
+            return redirect("index")
 
         form = CommentForm(request.POST, instance=comment)
 
         if form.is_valid():
             form.save()
-            return redirect('index')
+            return redirect("index")
 
         print(form.errors)
-        return render(request, 'comment.html', {'comment': comment})
+        return render(request, "comment.html", {"comment": comment})
