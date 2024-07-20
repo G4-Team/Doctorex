693e8b9e657fd9ed0c6be235f45d6281bea1bc01
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-20 20:08:39 +0330

    fix: recovery search feature

--- reservation/views.py
+++ reservation/views.py
@@ -1,9 +1,10 @@
 from django.contrib import messages
-from django.db.models import Avg
+from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
 
+from setting.models import Specialty
 from user.models import Doctor
 
 from .forms import CommentForm
@@ -15,15 +16,30 @@ class IndexView(View):
         return render(request, "reservation/index.html")
 
 
-class SearchView(View):
-    def get(self, request, search_content):
-        doctors = (
-            Doctor.objects.filter(account__first_name__icontains=search_content)
-            | Doctor.objects.filter(specialty__specialty__icontains=search_content)
-            | Doctor.objects.filter(account__last_name__icontains=search_content)
+class SerchView(View):
+    def get(self, request):
+        search = request.GET.get("search")
+        print(search)
+
+        specialties = Specialty.objects.filter(specialty__icontains=search)
+        doctors = Doctor.objects.filter(
+            Q(account__first_name__icontains=search)
+            | Q(account__last_name__icontains=search)
+        ).select_related("account")
+
+        context = {
+            "search": search,
+            "specialties": specialties,
+            "doctors": doctors,
+        }
+        from django.urls import reverse
+
+        print(reverse("account:signup"))
+        return render(
+            request=request,
+            template_name="reservation/search.html",
+            context=context,
         )
-        doctors.annotate(average_score=Avg("visittime__reservation__comments__score"))
-        return render(request, "reservation/search.html", {"doctors": doctors})
 
 
 class CommentView(View):
